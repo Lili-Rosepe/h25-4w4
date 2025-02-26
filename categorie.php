83515bd9cb2eaa7a8bf649389deedafa2c5d43e4
Perreault <e6253537@cmaisonneuve.qc.ca>
2025-02-26 16:44:22 -0500

    Avancement en classe 26 fev

--- categorie.php
+++ categorie.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
-    <h1>-------- INDEX.PHP ----------</h1>
+    <h1><?php single_cat_title(); ?> </h1>
     <section class="populaire">
         <div class="global">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article>
-                <h2><?php the_title(); ?></h2>
-                <div><?php the_content() ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <?php get_template_part( 'gabarits/carte' ); ?>
             <?php endwhile; endif; ?>
+       
         </div>
     </section>
     <?php get_footer(); ?>
